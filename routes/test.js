74ed2871ed886c75cbdd5050f7ed99e1200607f3
fybits <sergeig1313@gmail.com>
2020-03-24 13:32:33 +0300

    Update variable declaration with ES6 syntax

--- routes/test.js
+++ routes/test.js
@@ -1,27 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../database/models');
+const express = require('express');
+const router = express.Router();
+const db = require('../database/models');
 const Comment = db.comment;
 const Post = db.post;
-// var User = require('../database/models/comment')(db.sequelize, db.Sequelize.DataTypes)
 
-
-/* GET home page. */
 router.get('/', async function(req, res, next) {
-  var com = await Comment.findAll({ include: [Post, { model: Comment, as: 'comments'} ] });
+  let com = await Comment.findAll({ include: [Post, { model: Comment, as: 'comments'} ] });
   com = com[2];
   console.log(com.toJSON())
-  var post = await Post.findAll({ include: Comment });
+  let post = await Post.findAll({ include: Comment });
   post = post[1];
-  // console.log(post.toJSON());
-  // post.Comments.forEach(comment => {
-  //   console.log(comment.toJSON());
-  // });
-  // com.forEach(comment => {
-  //   console.log('-----------------------------');
-  //   console.log(comment);
-  //   console.log('-----------------------------');
-  // });
+
   const commentable = await com.getCommentable();
   res.render('index', { title: 'Express', content: JSON.stringify(commentable.toJSON())});
 });
