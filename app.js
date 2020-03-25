2b5bc556c7a96c0583984a7613a0c5bf6afd9890
fybits <sergeig1313@gmail.com>
2020-03-25 13:52:00 +0300

    Remove jade/pug from project

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,7 +38,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-  // render the error page
   res.status(err.status || 500);
   res.send('');
 });
